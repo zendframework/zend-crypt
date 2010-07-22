640d2c692adb6fc643bcec9fba61cc01a2ca74f2
Matthew Weier O'Phinney <matthew@zend.com>
2010-07-22 16:12:10 -0400

    HMAC -> Hmac; RSA -> Rsa
    - Updated all Crypt and OAuth libraries to convert:
      - HMAC -> Hmac
      - RSA -> Rsa

--- src/HmacException.php
+++ src/HmacException.php
@@ -32,6 +32,6 @@ namespace Zend\Crypt;
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class HMACException extends Exception
+class HmacException extends Exception
 {
 }
