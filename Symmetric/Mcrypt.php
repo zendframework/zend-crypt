eae9f8f4f377648efd72a201a76dff5c344da6c5
Marco Pivetta <ocramius@gmail.com>
2015-01-12 18:12:03 +0000

    Importing state as of zendframework/zf2@a9a025a39645543f50279e05713dd42de9d8bcc2 (1421086323)
    Automatic import via rsync

--- Symmetric/Mcrypt.php
+++ Symmetric/Mcrypt.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -239,7 +239,6 @@ class Mcrypt implements SymmetricInterface
          * the others are more picky.
          */
         if (!empty($keySizes) && $keyLen < $maxKey) {
-
             if (!in_array($keyLen, $keySizes)) {
                 throw new Exception\InvalidArgumentException(
                     "The size of the key must be one of " . implode(", ", $keySizes) . " bytes or longer"
