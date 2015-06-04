4e825ac092e72c500220b27b4684158de43212e8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-06-04 23:05:50 +0700

    replace uses of the static method ::hex2bin with the native function and remove the old method ::hex2bin

--- src/Key/Derivation/Scrypt.php
+++ src/Key/Derivation/Scrypt.php
@@ -44,7 +44,7 @@ abstract class Scrypt
             if ($length < 16) {
                 throw new Exception\InvalidArgumentException("Key length is too low, must be greater or equal to 16");
             }
-            return self::hex2bin(scrypt($password, $salt, $n, $r, $p, $length));
+            return hex2bin(scrypt($password, $salt, $n, $r, $p, $length));
         }
 
         $b = Pbkdf2::calc('sha256', $password, $salt, 1, $p * 128 * $r);
@@ -317,16 +317,5 @@ abstract class Scrypt
         }
         list(, $n) = unpack($v, substr($b, -64));
         return $n;
-    }
-
-    /**
-     * Convert hex string in a binary string
-     *
-     * @param  string $hex
-     * @return string
-     */
-    protected static function hex2bin($hex)
-    {
-        return hex2bin($hex);
     }
 }
