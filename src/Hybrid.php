d5955ca67af7752f954970085b119d87c2048247
Enrico Zimuel <e.zimuel@gmail.com>
2016-08-09 15:29:25 +0200

    Added the first draft of hybrid encryption

---
+++ src/Hybrid.php
@@ -0,0 +1,123 @@
+<?php
+namespace Zend\Crypt;
+
+use Zend\Math\Rand;
+use Zend\Crypt\PublicKey\Rsa\PublicKey as PubKey;
+use Zend\Crypt\PublicKey\Rsa\PrivateKey as PriKey;
+
+
+class Hybrid
+{
+    /**
+     * @var BlockCipher
+     */
+    protected $bCipher;
+
+    /**
+     * @var Rsa
+     */
+    protected $rsa;
+
+    /**
+     * Constructor
+     *
+     * @param BlockCipher $blockCipher
+     * @param Rsa $public
+     */
+    public function __construct(BlockCipher $bCipher = null, Rsa $rsa = null)
+    {
+        $this->bCipher = (null === $bCipher ) ? BlockCipher::factory('openssl') : $bCipher;
+        $this->rsa     = (null === $rsa ) ? new PublicKey\Rsa() : $rsa;
+    }
+
+    /**
+     * Encrypt using a keyrings
+     *
+     * @param string $plaintext
+     * @param array|string $keys
+     * @return string
+     * @throws RuntimeException
+     */
+    public function encrypt(string $plaintext, $keys = null)
+    {
+        // generate a random session key
+        $sessionKey = Rand::getBytes($this->bCipher->getCipher()->getKeySize());
+
+        // encrypt the plaintext with blockcipher algorithm
+        $this->bCipher->setKey($sessionKey);
+        $ciphertext = $this->bCipher->encrypt($plaintext);
+
+        if (null === $keys || is_string($keys)) {
+          $keys = [ '' => $keys ];
+        }
+
+        $encKeys = '';
+        // encrypt the session key with public keys
+        foreach ($keys as $id => $pubkey) {
+            if (is_string($pubkey)) {
+                $pubkey = new PubKey($pubkey);
+            } elseif (!($pubkey instanceof PubKey)) {
+                throw new Exception\RuntimeException(sprintf(
+                    "The public key must be an instance of %s", PubKey::class
+                ));
+            }
+            $encKeys .= sprintf(
+                "%s:%s:",
+                base64_encode($id),
+                base64_encode($this->rsa->encrypt($sessionKey, $pubkey))
+            );
+        }
+        return $encKeys . ';' . $ciphertext;
+    }
+
+    /**
+     * Decrypt usign a private key
+     *
+     * @param string $msg
+     * @param string $privateKey
+     * @param string $id
+     * @return string
+     * @throws RuntimeException
+     */
+    public function decrypt(string $msg, string $privateKey = null, string $id = '')
+    {
+        // get the session key
+        list($encKeys, $ciphertext) = explode(';', $msg, 2);
+
+        $keys = explode(':', $encKeys);
+        $pos  = array_search(base64_encode($id), $keys);
+        if (false === $pos) {
+            throw new Exception\RuntimeException(
+                "The private key is not valid"
+            );
+        }
+
+        $privKey = new PriKey($privateKey);
+        // decrypt the session key with privateKey
+        $sessionKey = $this->rsa->decrypt(base64_decode($keys[$pos + 1]), $privKey);
+
+        // decrypt the plaintext with the blockcipher algorithm
+        $this->bCipher->setKey($sessionKey);
+        return $this->bCipher->decrypt($ciphertext, $sessionKey);
+    }
+
+    /**
+     * Get the BlockCipher adapter
+     *
+     * @return BlockCipher
+     */
+    public function getBlockCipherInstance()
+    {
+        return $this->bCipher;
+    }
+
+    /**
+     * Get the Rsa instance
+     *
+     * @return Rsa
+     */
+    public function getRsaInstance()
+    {
+        return $this->rsa;
+    }
+}
---
+++ test/HybridTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Crypt;
+
+use Zend\Crypt\Hybrid;
+use Zend\Crypt\BlockCipher;
+use Zend\Crypt\PublicKey\Rsa;
+
+/**
+ * @group      Zend_Crypt
+ */
+class HybridTest extends \PHPUnit_Framework_TestCase
+{
+    protected $hybrid;
+
+    public function setUp()
+    {
+        if (!extension_loaded('openssl')) {
+          $this->markTestSkipped('The OpenSSL extension is required');
+        }
+        $this->hybrid = new Hybrid();
+    }
+
+    public function testConstructor()
+    {
+        $hybrid = new Hybrid();
+        $this->assertInstanceOf(Hybrid::class, $hybrid);
+    }
+
+    public function testGetDefaultBlockCipherInstance()
+    {
+        $bCipher = $this->hybrid->getBlockCipherInstance();
+        $this->assertInstanceOf(BlockCipher::class, $bCipher);
+    }
+
+    public function testGetDefaultRsaInstance()
+    {
+        $rsa = $this->hybrid->getRsaInstance();
+        $this->assertInstanceOf(Rsa::class, $rsa);
+    }
+
+    public function testEncryptDecryptWithOneKey()
+    {
+        $keys = openssl_pkey_new(array(
+            "private_key_bits" => 1024,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        ));
+
+        // Get the public and private key as string (PEM format)
+        $details   = openssl_pkey_get_details($keys);
+        $publicKey = $details['key'];
+        openssl_pkey_export($keys, $privateKey);
+
+        $result = $this->hybrid->encrypt('test', $publicKey);
+        $this->assertEquals('test', $this->hybrid->decrypt($result, $privateKey));
+    }
+
+    public function testEncryptWithMultipleKeys()
+    {
+
+    }
+
+}
