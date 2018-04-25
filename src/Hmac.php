cbcef1b9cb1f57138cc063527bf5a1a12a4eaa32
Matthew Weier O'Phinney <matthew@zend.com>
2018-04-25 09:53:19 -0500

    Fix too-long line as flagged by phpcs
    Assigns algorithms to a temporary variable, which is then referenced in
    the conditional, breaking up a too-long line.

--- src/Hmac.php
+++ src/Hmac.php
@@ -90,7 +90,8 @@ class Hmac
             return true;
         }
 
-        if (in_array(strtolower($algorithm), function_exists('hash_hmac_algos') ? hash_hmac_algos() : hash_algos(), true)) {
+        $algos = function_exists('hash_hmac_algos') ? hash_hmac_algos() : hash_algos();
+        if (in_array(strtolower($algorithm), $algos, true)) {
             static::$lastAlgorithmSupported = $algorithm;
             return true;
         }
